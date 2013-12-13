CyanogenMod/android_hardware_qcom_media

media: fix msm8660 omx issue
for msm8660: previous code lost libOmxVdec.so & libOmxVenc.so , now fixed it!

Change-Id: Ie6db1be912f0411bca47e9bfba9d0b7053febb18

Android.mk
@@ -14,7 +14,7 @@ ifneq ($(filter msm8610 msm8226 msm8974 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 endif
 
-ifneq ($(filter msm8610 msm8226 msm8974 msm8960 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8610 msm8226 msm8974 msm8660 msm8960 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 

TEAM-Gummy/hardware_qcom_media

Android.mk
@@ -15,7 +15,7 @@ ifneq ($(filter msm8610 msm8226 msm8974 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 endif
 
-ifneq ($(filter msm8610 msm8226 msm8974 msm8960 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 